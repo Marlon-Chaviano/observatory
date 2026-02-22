6bb42c160a47e7ad80cbbce7afac101efbe924a8
ViticoMC <victormanuelmartinezcampo178@gmail.com>
2026-02-22 09:01:16 -0500

    feat(auth): export RigthAddon and RoleSelector components in index file

--- src/features/auth/components/index.ts
+++ src/features/auth/components/index.ts
@@ -1,3 +1,6 @@
 export * from "./ErrorText";
+export * from "./InputField";
 export * from "./LoginForm";
 export * from "./RegisterForm";
+export * from "./RigthAddon";
+export * from "./RoleSelector";
